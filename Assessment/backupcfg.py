9fedc3f229e5d82e3a5996fdf2c97636e063cfab
CRSandler <30026663@students.sunitafe.edu.au>
2025-04-23 09:05:40 +1000

    Add files via upload

---
+++ Assessment/backupcfg.py
@@ -0,0 +1,7 @@
+source_folder = "/home/ec2-user/environment" #The directory where the files are located
+file_location = "/home/ec2-user/environment/PRG302_ASES/examples/TestFile.txt" #Location of a single file within source_location
+multiple_file_location = ['/PRG302_ASES/ICTPRG302_Python_features-Files.py', '/PRG302_ASES/ICTPRG302_Python_feature-Date_time_stamp.py', '/PRG302_ASES/Assessment/backup.log', '/mkkey.sh'] #Location of a multiple single file within source_location. Write in a list format
+directory_location = "/home/ec2-user/environment/PRG302_ASES/textfiles" #Location of a Directory within source_location
+backup_location = "/home/ec2-user/environment/PRG302_ASES/Assessment/Backups" #Where the backup is located
+backup_log_location = "/home/ec2-user/environment/PRG302_ASES/Assessment/backup.log" #Where the log file is stored
+
